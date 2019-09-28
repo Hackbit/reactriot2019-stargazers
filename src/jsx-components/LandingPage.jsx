171411d000462aca6e565bd741a97b3d2f44a059
Sana Ishaque <sanaishaqofficial@gmail.com>
2019-09-29 03:47:26 +0500

    wip

--- src/jsx-components/LandingPage.jsx
+++ src/jsx-components/LandingPage.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../css-components/LandinPage.css'
+import '../css-components/LandinPage.css';
+import Sound from 'react-sound';
 
 class LandingPage extends Component {
     render() {
         return (
-            <Link to="/MainPage"  className="Mainpage-container">
+            <div>
+            <Sound
+                // url="cool_sound.mp3"
+                // playStatus={Sound.status.PLAYING}
+                // playFromPosition={300 /* in milliseconds */}
+                // onLoading={this.handleSongLoading}
+                // onPlaying={this.handleSongPlaying}
+                // onFinishedPlaying={this.handleSongFinishedPlaying}
+            />
+            <Link to="/MainPage" className="Mainpage-container">
                 <div class="title">
                     <p>TODAY'S HOROSCOPE </p>
                 </div>
@@ -13,6 +23,7 @@ class LandingPage extends Component {
                     <p>"Astrology is a Language. If ou understand this language, The Sky Speaks to You." -Dane Rudhyar</p>
                 </div>
             </Link>
+            </div>
         );
     }
 }
