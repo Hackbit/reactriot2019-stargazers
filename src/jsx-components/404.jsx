c9a870de00ac8c997b03c0a6b4271743fd05d46c
Sana Ishaque <sanaishaqofficial@gmail.com>
2019-09-28 21:21:01 +0500

    component 404 created
    linked  to landing page

--- src/jsx-components/404.jsx
+++ src/jsx-components/404.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../css-components/PageNotFound.css';
+import { Link } from 'react-router-dom';
 
-function PageNotFound() {
-    return (
-        <div>
-            <p>PAGE NOT FOUND</p>
-        </div>
-    );
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <div className="Error">
+                <h1 className="PageNotFound">404</h1>
+                <p>Something went wrong</p>
+                <p>We're deeply sorry, but something went wrong. Please try to refresh the page or </p>
+                <Link to="/"><p>start over</p></Link>
+            </div>
+        );
+    }
 }
 
 export default PageNotFound;
