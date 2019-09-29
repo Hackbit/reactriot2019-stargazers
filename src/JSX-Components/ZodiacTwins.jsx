2c3e0909f792cad826c3232b4aa237923cb0e228
Syeda-Taliya <49196177+SyedaTaliya@users.noreply.github.com>
2019-09-29 16:22:57 +0500

    ...

--- src/JSX-Components/ZodiacTwins.jsx
+++ src/JSX-Components/ZodiacTwins.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import '../css-components/ZodiacTwins.css';
-
-
+import './ZodiacTwins.css';
 
 const ZodiacTwins = props => {
     return (
 
-    <div className="zodiac-stars">
-
-        <div className="main-heading">
-            {/* <h1>{props.zodiacName}</h1> */}
-            {/* <img  src={props.zodiacSign} alt="" />  */}
+        <div className="body">
+        <div className="box">
+        <div className="card">
+          <div className="imgBx">
+             <img src={props.personalityImage} alt="PersonalitiesImgs" />
+          </div>
+          <div className="details">
+              <h2>{props.personalityName}</h2>
+          </div>
         </div>
 
-        <h6>{props.date}</h6>
-
-        <div>
-            <img className="celebs" src={props.personalityImage} alt="PersonalitiesImgs" />
-            <p>{props.personalityName}</p>
         </div>
+        </div>
+        
 
-    </div>
+       
+    
 
     );
 };
