59f258ca8ffee43b3b9509faf3a3204b299c8f2b
Sana Ishaque <sanaishaqofficial@gmail.com>
2019-09-30 02:21:20 +0500

    Background Music Added
    Home tab aded in header.
    font changes on main page.

--- src/jsx-components/Header.jsx
+++ src/jsx-components/Header.jsx
@@ -6,9 +6,10 @@ class Header extends Component {
     render() {
         return (
             <div className="Nav">
+            <Link to="/"><p>Home</p></Link>
             <Link to="/ZodiacSigns"><p>All About Start Signs</p></Link>
             <Link to="/404"><p>Your Star Twin</p></Link>
-            <Link to="/404"><p>Book for your Star Sign</p></Link>
+            <Link to="/404"><p>Book For Your Star Sign</p></Link>
         </div>
         );
     }
